34c483ec82e950ecdd04426b45037e1a7760f398
basst85 <bastiaan85@gmail.com>
2018-09-26 21:55:04 +0200

    Fix barcode.js

--- barcode.js
+++ barcode.js
@@ -1,55 +1,60 @@
 var video;
 var canvas;
-var barcodeDetector;
-var barcodeValue;
-var barcodeBox;
+var detectedBarcodes;
 var errorDiv;
+var barcodeDetector;
 
 function setup() {
 	noCanvas();
 	
 	errorDiv = document.getElementById('error');
 	canvas = document.getElementById('canvas');
-	
+	video = document.getElementById('video');
+
 	var constraints = {video: {facingMode: 'environment'}};
 	navigator.mediaDevices.getUserMedia(constraints)
 		.then(function(mediaStream) {
-			var video = document.getElementById('video');
 			video.srcObject = mediaStream;
 			video.onloadedmetadata = function(e) {
 				video.play();
+				canvas.width = video.videoWidth;
+				canvas.height = video.videoHeight;
 			};
 		})
-		.catch(function(err) { errorDiv.innerText = err.name + ': ' + err.message; }); 
+		.catch(function(err) {  }); 
 }
 
 function draw() {	
-	if(typeof barcodeBox !== 'undefined'){
-		var ctx = canvas.getContext('2d');
-		ctx.beginPath();
-		ctx.lineWidth='6';
-		ctx.strokeStyle='red';
-		ctx.rect(barcodeBox.x,barcodeBox.y,barcodeBox.width,barcodeBox.height); 
-		ctx.stroke();
-		ctx.font='20px Arial';
-		ctx.fillStyle = 'red';
-		ctx.fillText(barcodeValue,barcodeBox.x-5,(barcodeBox.y+barcodeBox.height+20));
+	if(typeof detectedBarcodes !== 'undefined'){
+		for(barcode of detectedBarcodes){
+			var ctx = canvas.getContext('2d');
+			ctx.beginPath();
+			ctx.lineWidth='6';
+			ctx.strokeStyle='red';
+			ctx.rect(barcode.boundingBox.x,barcode.boundingBox.y,barcode.boundingBox.width,barcode.boundingBox.height); 
+			ctx.stroke();
+			ctx.font='20px Arial';
+			ctx.fillStyle = 'red';
+			ctx.fillText(barcode.rawValue,barcode.boundingBox.x-5,(barcode.boundingBox.y+barcode.boundingBox.height+20));
+		}
 	}
 }
 	
-async function getBarcode(){
-	try {
-		barcodeDetector = new BarcodeDetector();
-		const barcodes = await barcodeDetector.detect(canvas);
-		barcodes.forEach(barcode => {
-			barcodeValue = barcode.rawValue;
-			barcodeBox = barcode.boundingBox;
-		});
-	} catch (exception) {
-		errorDiv.innerText = exception;
+async function getBarcodes(){
+	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+	
+	if(!video.paused){
+		try{
+			barcodeDetector = new BarcodeDetector();
+			const barcodes = await barcodeDetector.detect(canvas);
+			detectedBarcodes = barcodes;
+		}
+		catch(exception) {
+			errorDiv.innerText = exception;
+		};
 	}
 }
 
 setInterval(function(){ 
-	getBarcode() 
-}, 100);
+	getBarcodes() 
+}, 200);
